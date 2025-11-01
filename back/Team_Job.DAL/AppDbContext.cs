847b1cd95942855c122ca1ea450b85dcad675213
SSkk999 <cvantymgeymshhhhh@gmail.com>
2025-11-01 17:36:18 +0200

    Базова структура

---
+++ back/Team_Job.DAL/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Team_Job.DAL
+{
+    public class AppDbContext :DbContext
+    {
+        public  AppDbContext(DbContextOptions options) : base(options) { }
+    }
+}
