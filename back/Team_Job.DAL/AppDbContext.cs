6ea98545a854ceb78f0886fd9239388febffdd61
SamkovLuka <staddycase@gmail.com>
2025-11-02 19:46:09 +0200

    Added entities and relations in appdbcontext

--- back/Team_Job.DAL/AppDbContext.cs
+++ back/Team_Job.DAL/AppDbContext.cs
@@ -1,9 +1,31 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Team_Job.DAL.Entities;
 
 namespace Team_Job.DAL
 {
     public class AppDbContext :DbContext
     {
         public  AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<BookingEntity> Bookings { get; set; }
+        public DbSet<HouseEntity> Houses { get; set; }
+        public DbSet<UserEntity> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<BookingEntity>()
+                .HasOne(b => b.House)
+                .WithMany(h => h.Bookings)
+                .HasForeignKey(b => b.HouseId);
+
+
+            modelBuilder.Entity<BookingEntity>()
+                .HasOne(b => b.User)
+                .WithMany(u => u.Bookings)
+                .HasForeignKey(b => b.UserId);
+                
+        }
     }
 }
---
+++ back/Team_Job.DAL/Entities/BookingEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_Job.DAL.Entities
+{
+    public class BookingEntity
+    {
+        public int Id { get; set; }
+        public int HouseId { get; set; }
+        public HouseEntity? House { get; set; }
+        public int UserId { get; set; }
+        public UserEntity? User { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
