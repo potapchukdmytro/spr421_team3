0b05d21f22b3af2e050b21a0a9170ed0cc9e657e
SSkk999 <cvantymgeymshhhhh@gmail.com>
2025-11-11 03:11:43 +0200

    fix problem

--- back/Team_Job/Program.cs
+++ back/Team_Job/Program.cs
@@ -1,8 +1,11 @@
 
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Team_Job.BLL.Services.House;
 using Team_Job.BLL.Services.Storage;
 using Team_Job.DAL;
+using Team_Job.DAL.Entities.Identity;
+using Team_Job.DAL.Initializer;
 using Team_Job.DAL.Repositories.Booking;
 using Team_Job.DAL.Repositories.House;
 using Team_Job.DAL.Repositories.User;
@@ -17,6 +20,32 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultDb"));
 });
+
+// Add identity
+
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+
+{
+
+    options.User.RequireUniqueEmail = true;
+
+    options.Password.RequiredUniqueChars = 0;
+
+    options.Password.RequireNonAlphanumeric = false;
+
+    options.Password.RequireDigit = false;
+
+    options.Password.RequireLowercase = false;
+
+    options.Password.RequireUppercase = false;
+
+    options.Password.RequiredLength = 6;
+
+})
+
+    .AddDefaultTokenProviders()
+
+    .AddEntityFrameworkStores<AppDbContext>();
 
 // Add automapper
 builder.Services.AddAutoMapper(options =>
@@ -53,5 +82,7 @@ app.UseAuthorization();
 app.AddStaticFiles(app.Environment);
 
 app.MapControllers();
+
+app.Seed();
 
 app.Run();
