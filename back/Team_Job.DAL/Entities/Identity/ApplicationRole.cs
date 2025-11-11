bc6bb23ec9b0e8be691e0d293aee923b54a0fee9
SSkk999 <cvantymgeymshhhhh@gmail.com>
2025-11-11 02:41:40 +0200

    Додано сидер та оновлено сутності

---
+++ back/Team_Job.DAL/Entities/Identity/ApplicationRole.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Team_Job.DAL.Entities.Identity
+{
+    public class ApplicationRole : IdentityRole
+    {
+        public virtual ICollection<ApplicationUserRole> UserRoles { get; set; } = [];
+        public virtual ICollection<ApplicationRoleClaim> RoleClaims { get; set; } = [];
+    }
+}
---
+++ back/Team_Job.DAL/Settings/RoleSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Team_Job.DAL.Settings
+{
+    public static class RoleSettings
+    {
+        public const string RoleAdmin = "admin";
+        public const string RoleUser = "user";
+    }
+}
